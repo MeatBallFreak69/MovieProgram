af6eeb6577303c7ec915a0aa0979e73a5407a708
Clark Miller <glitchclick@gmail.com>
2021-02-17 11:10:08 -0500

    Add project files.

---
+++ MovieProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Clark";
+            string favMovie = "The Matrix.".ToUpper();
+
+            Console.WriteLine($"Hello, my name is {firstName} and my favourite movie is {favMovie}".ToLower());
+            Console.WriteLine(favMovie.Contains("THE"));
+
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+            Console.WriteLine($"Hello, my name is {firstName} and my favourite movie is {favMovie}".ToLower());
+        }
+    }
+}
